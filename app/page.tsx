b86aa999b37a4ae7386c8a2b53363b7cd8316408
Scott Greff <scottiegreff@gmail.com>
2024-08-10 12:08:12 -0700

    add object-contain to preserve aspect ratio for responsivness

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,29 @@ import services from "@/public/services.png";
 import addOns from "@/public/add-ons.png";
 import addOns2 from "@/public/add-ons2.png";
 
-
 export default function Home() {
   return (
     <main className="flex gap-5 min-h-screen flex-col items-center justify-between bg-gradient-to-b from-[#e3edde] to-[#193d41] p-10">
-     <Image className="h-[97vh] w-fit rounded-2xl drop-shadow-2xl" src={poster} alt="logo" width={200} height={200} />
-     <Image className="h-[97vh] w-fit rounded-2xl drop-shadow-2xl" src={services} alt="logo" width={200} height={200} />
-     <Image className="h-[97vh] w-fit rounded-2xl drop-shadow-2xl" src={addOns} alt="logo" width={200} height={200} />
-     <Image className="h-[97vh] w-fit rounded-2xl drop-shadow-2xl" src={addOns2} alt="logo" width={200} height={200} />
+      <Image
+        className="h-[97vh] w-auto max-h-screen object-contain rounded-2xl drop-shadow-2xl"
+        src={poster}
+        alt="logo"
+      />
+      <Image
+        className="h-[97vh] w-auto max-h-screen object-contain rounded-2xl drop-shadow-2xl"
+        src={services}
+        alt="logo"
+      />
+      <Image
+        className="h-[97vh] w-auto max-h-screen object-contain rounded-2xl drop-shadow-2xl"
+        src={addOns}
+        alt="logo"
+      />
+      <Image
+        className="h-[97vh] w-auto max-h-screen object-contain rounded-2xl drop-shadow-2xl"
+        src={addOns2}
+        alt="logo"
+      />
     </main>
   );
 }
