bf8b1b8a09c1817e2b08826ce3ca4567ba604d33
Scott Greff <scottiegreff@gmail.com>
2024-08-16 18:11:16 -0700

    update the font size of service and addons pages

--- components/AddOns.tsx
+++ components/AddOns.tsx
@@ -7,24 +7,24 @@ const AddOns = () => {
         {" "}
         Treatment Add-ons:
       </h2>
-      <h2 className="uppercase text-white text-3xl font-md text-center">
+      <h2 className="uppercase text-white text-xl lg:text-3xl font-md text-center">
         {" "}
         Face Steam (Swedana)
       </h2>
-      <p className="uppercase text-white text-lg font-light text-center">
+      <p className="uppercase text-white text-md lg:text-lg font-light text-center">
         Prepares skin for face massage. Detoxifying, Alleviates congestion and
         allergies, Sharpens mental clarity and promotes relaxation.
       </p>
-      <p className="uppercase text-white text-lg text-center">
+      <p className="uppercase text-white text-md lg:text-lg text-center">
         Citrus Face steam Grapefruit, Sweet Orange, Lemon
       </p>
-      <p className="uppercase text-white text-lg text-center">
+      <p className="uppercase text-white text-md lg:text-lg text-center">
         Clarifying Face Steam Lavender and Tea Tree
       </p>{" "}
-      <p className="uppercase text-white text-lg text-center">
+      <p className="uppercase text-white text-md lg:text-lg text-center">
         Balancing Face Steam ylang ylang, geranium, frankincense
       </p>
-      <h2 className="uppercase text-white text-3xl font-semibold text-center">
+      <h2 className="uppercase text-white text-xl lg:text-3xl font-semibold text-center">
         Rosemary + Coconut OIl Scalp Massage
       </h2>{" "}
     </div>
--- components/Services.tsx
+++ components/Services.tsx
@@ -3,33 +3,54 @@ import React from "react";
 const Services = () => {
   return (
     <div className="h-[93vh] w-[93vw] max-w-[595px] flex flex-col justify-between items-center  mt-10 border-white border-4 rounded-lg p-7">
-      <h2 className="uppercase text-white text-2xl lg:text-3xl font-semibold text-center">
-        {" "}
-        BRINGING RELAXATION
+      <div>
+        <h2 className="uppercase text-white text-2xl lg:text-3xl font-semibold text-center mb-3">
+          {" "}
+          BRINGING RELAXATION
+        </h2>
+        <h2 className="uppercase text-white text-xl lg:text-3xl font-semibold text-center">
+          {" "}
+          TO YOUR HOME
+        </h2>
+      </div>
+      <h2 className="uppercase text-white text-xl lg:text-2xl text-center">
+        Mobile SERVICES:
       </h2>
-      <h2 className="uppercase text-white text-lg lg:text-3xl font-semibold text-center">
-        {" "}
-        TO YOUR HOME
-      </h2>
-      <h2 className="uppercase text-white text-lg lg:text-2xl text-center">Mobile SERVICES:</h2>
-      <h2 className="uppercase text-white text-lg lg:text-2xl text-center">SWEDISH </h2>
-      <p className="uppercase text-white text-md lg:text-lg text-center">
-        for relaxation and circulation
-      </p>
-      <h2 className="uppercase text-white text-lg lg:text-2xl text-center">DEEP TISSUE</h2>
-      <p className="uppercase text-white text-md lg:text-lg text-center">for tension and knots</p>
-      <h2 className="uppercase text-white text-lg lg:text-2xl text-center">DEEP FLOW</h2>{" "}
-      <p className="uppercase text-white text-md lg:text-lg text-center">
-        a blend of relaxation and knot release
-      </p>{" "}
-      <h2 className="uppercase text-white text-lg lg:text-2xl font-semibold">
-        The Bedtime Ritual
-      </h2>{" "}
-      <p className="uppercase text-white text-md lg:text-lg text-center">
-        With sedative inducing Valerian oil, an acupressure Marma Point Massage
-        for face scalp and neck, and guided breath work to help you attain a
-        state of deep relaxation before you dose off peacefully to sleep.
-      </p>
+      <div>
+        <h2 className="uppercase text-white text-xl lg:text-2xl text-center">
+          SWEDISH{" "}
+        </h2>
+        <p className="uppercase text-white text-md lg:text-lg text-center">
+          for relaxation and circulation
+        </p>
+      </div>
+      <div>
+        <h2 className="uppercase text-white text-xl lg:text-2xl text-center">
+          DEEP TISSUE
+        </h2>
+        <p className="uppercase text-white text-md lg:text-lg text-center">
+          for tension and knots
+        </p>
+      </div>
+      <div>
+        <h2 className="uppercase text-white text-lg lg:text-2xl text-center">
+          DEEP FLOW
+        </h2>{" "}
+        <p className="uppercase text-white text-md lg:text-lg text-center">
+          a blend of relaxation and knot release
+        </p>{" "}
+      </div>
+      <div>
+        <h2 className="uppercase text-white text-lg lg:text-2xl font-semibold text-center">
+          The Bedtime Ritual
+        </h2>{" "}
+        <p className="uppercase text-white text-md lg:text-lg text-center">
+          With sedative inducing Valerian oil, an acupressure Marma Point
+          Massage for face scalp and neck, and guided breath work to help you
+          attain a state of deep relaxation before you dose off peacefully to
+          sleep.
+        </p>
+      </div>
     </div>
   );
 };
